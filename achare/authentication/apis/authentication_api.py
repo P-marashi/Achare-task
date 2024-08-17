8d90b43cf1b8587ac43e2f9bb6d141d2a2bfe9da
P-marashi <pouyamarashii@gmail.com>
2024-08-17 17:57:30 +0330

    🦺 Remove extra validate serializer

--- achare/authentication/apis/authentication_api.py
+++ achare/authentication/apis/authentication_api.py
@@ -1,16 +1,8 @@
 import logging
 from typing import Any
-from rest_framework import status
 
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from achare.authentication.services.login_services import login_user
-from achare.authentication.services.profile_services import update_user_profile
-from achare.authentication.helper_functions import get_client_ip, is_blocked
-from achare.core.messages import TOO_MANY_FAILED_ATTEMPTS
-from drf_spectacular.utils import extend_schema
+from achare.authentication.auth_blocking import is_blocked
+from achare.authentication.helper_functions import get_client_ip
 from achare.authentication.services.authentication_services import (
     authenticate_user_by_mobile,
 )
@@ -18,6 +10,27 @@ from achare.authentication.services.create_verify_services import (
     handle_new_user_authentication,
     verify_and_authenticate_user,
 )
+from achare.authentication.services.login_services import login_user
+from achare.authentication.services.profile_services import update_user_profile
+from achare.core.exceptions import (
+    InvalidNonceException,
+    InvalidOTPException,
+    InvalidPasswordException,
+    UserBlockedException,
+)
+from achare.core.messages import (
+    INTERNAL_SERVER_ERROR,
+    INVALID_OR_EXPIRED_NONCE,
+    INVALID_OR_EXPIRED_OTP,
+    INVALID_PASSWORD_OR_USERNAME,
+    PROFILE_UPDATED_SUCCESSFULLY,
+)
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import (
     AuthenticationSerializer,
@@ -32,48 +45,59 @@ logger.setLevel(logging.DEBUG)
 
 class UserAuthentication(APIView):
     """
-    Handle user authentication by
-    checking if the user exists and
-    sending OTP if needed.
+    Handle user authentication by checking if the user exists
+    and sending an OTP if needed.
     """
 
     @extend_schema(tags=["account"], request=AuthenticationSerializer)
     def post(self, request: Any) -> Response:
         serializer = AuthenticationSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            ip_address = get_client_ip(request)
-            mobile_number: str = serializer.validated_data["mobile_number"]
-            user, nonce = authenticate_user_by_mobile(mobile_number)
-            is_user_blocked, remaining_time = is_blocked(ip_address, mobile_number)
-            if is_user_blocked:
-                return Response(
-                    {
-                        "detail": TOO_MANY_FAILED_ATTEMPTS.format(time=remaining_time),
-                    },
-                    status=status.HTTP_403_FORBIDDEN,
-                )
-            if user:
-                response_data = {
-                    "its_new_user": False,
-                    "nonce": nonce,
-                    "login_method": user.login_method,
-                }
-                return Response(response_data, status=status.HTTP_200_OK)
+        serializer.is_valid(raise_exception=True)
 
-            return handle_new_user_authentication(mobile_number)
+        ip_address = get_client_ip(request)
+        mobile_number: str = serializer.validated_data["mobile_number"]
+        is_user_blocked, remaining_time = is_blocked(ip_address, mobile_number)
+        if is_user_blocked:
+            raise UserBlockedException(remaining_time=remaining_time)
+        user, nonce = authenticate_user_by_mobile(mobile_number)
+
+        if user:
+            response_data = {
+                "its_new_user": False,
+                "nonce": nonce,
+                "login_method": user.login_method,
+            }
+            return Response(response_data, status=status.HTTP_200_OK)
+
+        new_user_data = handle_new_user_authentication(mobile_number)
+        return Response(new_user_data, status=status.HTTP_200_OK)
 
 
 class VerifyOtp(APIView):
-    """Verify the OTP provided by the user to authenticate him."""
+    """Verify the OTP provided by the user to authenticate them."""
 
     @extend_schema(tags=["account"], request=VerifyOTPSerializer)
     def post(self, request: Any) -> Response:
         ip_address = get_client_ip(request)
         serializer = VerifyOTPSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            nonce = serializer.validated_data["nonce"]
-            otp = serializer.validated_data["otp"]
-            return verify_and_authenticate_user(nonce, otp, ip_address)
+        serializer.is_valid(raise_exception=True)
+
+        nonce = serializer.validated_data["nonce"]
+        otp = serializer.validated_data["otp"]
+
+        try:
+            tokens = verify_and_authenticate_user(nonce, otp, ip_address)
+            return Response(tokens, status=status.HTTP_200_OK)
+        except UserBlockedException as e:
+            return Response({"detail": str(e)}, status=status.HTTP_403_FORBIDDEN)
+        except InvalidNonceException:
+            return Response(
+                {"detail": INVALID_OR_EXPIRED_NONCE}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except InvalidOTPException:
+            return Response(
+                {"detail": INVALID_OR_EXPIRED_OTP}, status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class CompleteProfile(APIView):
@@ -85,22 +109,50 @@ class CompleteProfile(APIView):
     def put(self, request: Any) -> Response:
         user = request.user
         serializer = CompleteProfileSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            return update_user_profile(user, serializer)
+        serializer.is_valid(raise_exception=True)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            update_user_profile(user, serializer)
+            return Response(
+                {"detail": PROFILE_UPDATED_SUCCESSFULLY}, status=status.HTTP_200_OK
+            )
+        except ValidationError as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            logger.error(f"Error in profile update: {e}")
+            return Response(
+                {"detail": f"{INTERNAL_SERVER_ERROR}: {str(e)}"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
 
 class LoginUser(APIView):
-    """login user with there password or if they dont have password with otp"""
+    """Log in user with their password or OTP."""
 
     @extend_schema(tags=["account"], request=LoginUserSerializer)
-    def post(self, request):
+    def post(self, request: Any) -> Response:
         ip_address = get_client_ip(request)
         serializer = LoginUserSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            nonce = serializer.validated_data["nonce"]
-            password = serializer.validated_data["password"]
-            return login_user(nonce, password, ip_address)
+        serializer.is_valid(raise_exception=True)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        nonce = serializer.validated_data["nonce"]
+        password = serializer.validated_data["password"]
+
+        try:
+            tokens = login_user(nonce, password, ip_address)
+            return Response(tokens, status=status.HTTP_200_OK)
+        except InvalidNonceException:
+            return Response(
+                {"detail": INVALID_OR_EXPIRED_NONCE}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except UserBlockedException as e:
+            return Response({"detail": str(e)}, status=status.HTTP_403_FORBIDDEN)
+        except InvalidOTPException:
+            return Response(
+                {"detail": INVALID_OR_EXPIRED_OTP}, status=status.HTTP_400_BAD_REQUEST
+            )
+        except InvalidPasswordException:
+            return Response(
+                {"detail": INVALID_PASSWORD_OR_USERNAME},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
--- achare/authentication/apis/serializers.py
+++ achare/authentication/apis/serializers.py
@@ -1,5 +1,6 @@
-from achare.utils.validators import validate_phone_number
+from achare.utils.regexes import phone_number_regex
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -7,10 +8,11 @@ class AuthenticationSerializer(serializers.Serializer):
     mobile_number = serializers.CharField(max_length=11)
 
     def validate_mobile_number(self, value):
-        if not value.startswith("09"):
-            raise ValidationError("Phone number must start with '09'.")
-        if len(value) != 11:
-            raise ValidationError("Phone number must be exactly 11 digits.")
+        # Use regex to validate the phone number
+        if not phone_number_regex(value):
+            raise ValidationError(
+                _("Phone number must start with '09' and be 11 digits long.")
+            )
         return value
 
 
@@ -21,7 +23,7 @@ class VerifyOTPSerializer(serializers.Serializer):
     def validate_otp(self, value):
         # Ensure the OTP is exactly 6 characters long
         if len(value) != 6:
-            raise ValidationError("OTP must be exactly 6 digits.")
+            raise ValidationError(_("OTP must be exactly 6 digits."))
         return value
 
 
